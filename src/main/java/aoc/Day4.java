b041eb694ee80cc9ead25103ff716c6b530f6def
Kelton Finch <keltonfinch@gmail.com>
2018-12-19 08:47:07 -0500

    Added Day4 starter

---
+++ src/main/java/aoc/Day4.java
@@ -0,0 +1,19 @@
+package aoc;
+
+import java.util.Scanner;
+
+public class Day4 {
+
+    public static void main (String args[]) {
+        try {
+            day4_1();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void day4_1() {
+        Scanner sc = Utils.getScanner("day4input.txt");
+    }
+
+}
